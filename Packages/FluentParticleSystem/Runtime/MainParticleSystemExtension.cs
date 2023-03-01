a0962f53d0defc98378320c5678f8b478fb6e33c
yu195 <miyayuu.since2018@gmail.com>
2023-03-01 18:35:38 +0900

    サンプルを追加

---
+++ Packages/FluentParticleSystem/Runtime/MainParticleSystemExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace FluentParticleSystem
+{
+    public static class MainParticleSystemExtension
+    {
+        public static ParticleSystem SetMainDuration(this ParticleSystem particleSystem, float duration) {
+            ThrowHelper.ThrowArgumentNullExceptionIfNull(particleSystem, nameof(particleSystem));
+            var main = particleSystem.main;
+            main.duration = duration;
+            return particleSystem;
+        }
+
+        public static ParticleSystem SetMainDuration(this ParticleSystem particleSystem, Func<float, float> durationChanger) {
+            ThrowHelper.ThrowArgumentNullExceptionIfNull(particleSystem, nameof(particleSystem));
+            var main = particleSystem.main;
+            main.duration = durationChanger(main.duration);
+            return particleSystem;
+        }
+
+        public static ParticleSystem EditMain(this ParticleSystem particleSystem, Action<ParticleSystem.MainModule> mainModuleEditor) {
+            ThrowHelper.ThrowArgumentNullExceptionIfNull(particleSystem, nameof(particleSystem));
+            mainModuleEditor(particleSystem.main);
+            return particleSystem;
+        }
+
+        public static ParticleSystem.MainModule SetDuration(this ParticleSystem.MainModule mainModule, float duration) {
+            mainModule.duration = duration;
+            return mainModule;
+        }
+    }
+}
