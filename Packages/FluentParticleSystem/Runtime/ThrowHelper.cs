16eed81580ac57f146e78c425209d878732d1098
yu195 <miyayuu.since2018@gmail.com>
2023-03-01 16:35:47 +0900

    asmdefを追加

---
+++ Packages/FluentParticleSystem/Runtime/ThrowHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentParticleSystem
+{
+    internal static class ThrowHelper
+    {
+        public static void ThrowArgumentNullExceptionIfNull(object parameter, string paramName) {
+            if (parameter is null)
+                throw new ArgumentNullException(paramName);
+        }
+    }
+}
